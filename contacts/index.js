b557a0068dffaa41d81e7bda19a4acfb75b7c057
Suttikiat Moongkhunthod <57160664@go.buu.ac.th>
2017-12-14 18:19:38 +0700

    Last 6 Complete

--- contacts/index.js
+++ contacts/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 let contactList = [
-    {id: 0, name: 'Ned Stark', email: 'ned@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Winter is coming.'},
-    {id: 1, name: 'Theon Greyjoy', email: 'tgreyjoy@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Reluctant to pay iron price.'},
-    {id: 2, name: 'Samwell Tarly', email: 'starly@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Loyal brother of the watch.'},
-    {id: 3, name: 'Jon Snow', email: 'jsnow@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Knows nothing.'},
-    {id: 4, name: 'Arya Stark', email: 'waterdancer@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Has a list of names.'},
+    {id: 0, name: 'Kong', email: 'ned@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Winter is coming.'},
+    {id: 1, name: 'Theon', email: 'tgreyjoy@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Reluctant to pay iron price.'},
+    {id: 2, name: 'Samwell', email: 'starly@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Loyal brother of the watch.'},
+    {id: 3, name: 'Snow', email: 'jsnow@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Knows nothing.'},
+    {id: 4, name: 'Stark', email: 'waterdancer@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Has a list of names.'},
     {id: 5, name: 'Jora Mormont', email: 'khaleesifan100@gmail.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Lost in the friend-zone.'},
     {id: 6, name: 'Tyrion Lannister', email: 'tyrion@lannister.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Currently drunk.'},
     {id: 7, name: 'Stannis Baratheon', email: 'onetrueking@dragonstone.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Nobody expects the Stannish inquisition.'},
@@ -17,9 +17,23 @@ let contactList = [
   ];
 
 //Develop GET /contacts API to list all contacts
-router.get('/contacts', (req, res) => {
-    res.json(contactList)
+//Develop GET /contacts?name= API to search contact by name
+
+  router.get('/contacts/',(req,res) =>{
+    if(req.query.name){
+        for(i=0;i<contactList.length;i++)
+            if(contactList[i].name == req.query.name)
+                res.json(contactList[i])
+    }
+    else{
+        res.json(contactList)
+    }
 })
+
+//Develop GET /contacts API to list all contacts
+/*router.get('/contacts', (req, res) => {
+    res.json(contactList)
+})*/
 
 //Develop GET /contact/:id API to get contact information
 router.get('/contacts/:id', (req, res) => {
@@ -49,5 +63,8 @@ router.delete('/contacts/:id', (req, res) => {
     contactList.splice(id, 1)
     res.status(204).json()
 })
+
+
+
 module.exports = router
 
